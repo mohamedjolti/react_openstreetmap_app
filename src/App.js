5657c3fd7030f7678be36107f543290e03ca6ca7
Mohamed JOLTI <mohamedelkholti062@gmail.com>
2020-10-31 14:24:16 +0100

    wheater working fine

--- src/App.js
+++ src/App.js
@@ -4,9 +4,16 @@ import { Map, TileLayer } from "react-leaflet";
 import L from "leaflet";
 
 const height = { height: "100vh" };
-const center = { lat: 51.5, lng: 0.12 };
+const center = { lat: 35.770390, lng: -5.803610 };
 
 class MapExample extends Component {
+
+  state={
+        city:"Tangier",
+        temperature:295.15,
+        pressure:1024,
+        humidity:64
+  }
   componentDidMount() {
     const map = this.leafletMap.leafletElement; 
     const geocoder = L.Control.Geocoder.nominatim();
@@ -19,16 +26,39 @@ class MapExample extends Component {
         results => {
           var r = results[0];
           console.log(r);
-          var html =r+"<button> click here</button> "
+          this.setState({
+            city:r.properties.address.city
+          })
+          console.log(r.properties.address.city);
+          const city=r.properties.address.city
+          if(city){
+          fetch(`https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?q=${r.properties.address.city}&appid=bb8db9a16f62b122752372804974e78d`,{
+            headers:new Headers({
+              appid:process.env.REACT_APP_APP_ID
+            })
+          })
+        .then(response =>response.json() ).then(data=>{
+            console.log(data);
+          this.setState({
+            temperature:data.main.temp,
+            pressure:data.main.pressure,
+            humidity:data.main.humidity
+          })
+          
+        })
+.catch(err => {
+	console.log(err);
+});
+          }
           if (r) {
             if (marker) {
               marker
                 .setLatLng(r.center)
-                .setPopupContent(r.html || r.name)
+                .setPopupContent("you choosed the city  :" +this.state.city +  ", the temperature in this city " +this.state.temperature + " and the pressure  "+ this.state.pressure)
                 .openPopup();
             } else {
               marker = L.marker(r.center)
-                .bindPopup(r.name)
+                .bindPopup("No city selected")
                 .addTo(map)
                 .openPopup();
             }
